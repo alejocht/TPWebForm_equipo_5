a5e94107d70aeea9ef906209a9163be38570ba8a
Mariano Stoessel <mariansto10@gmail.com>
2024-05-14 07:24:30 -0300

    se acomodo la pagina

--- VentanaProductos.aspx.designer.cs
+++ VentanaProductos.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace TPWebForm_equipo_5
 
     public partial class VentanaProductos
     {
+
+        /// <summary>
+        /// Control DdlOrden.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlOrden;
     }
 }
