a52fbb2bbd99abd3c3c1c70f57d5abdb234c5fc5
Mariano Stoessel <mariansto10@gmail.com>
2024-05-17 20:51:35 -0300

    Pasaje de datos por session
    Tuve que cambiar la forma de cargar los datos en vez de un foreach use repeat para poder pasarme el id del producto al cs

--- VentanaProductos.aspx
+++ VentanaProductos.aspx
@@ -5,38 +5,38 @@
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    
+
     <div class="container text-center">
         <h1>Productos</h1>
     </div>
 
 
     <div class="container d-flex justify-content-end" style="margin-right: 10px">
-        <asp:DropDownList ID="DdlOrden" runat="server"
+        <asp:DropDownList ID="DdlOrden" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DdlOrden_SelectedIndexChanged"
             aria-label="Default select example">
-            
         </asp:DropDownList>
     </div>
 
     <div class="container text-center">
-    <div class="row row-cols-1 row-cols-md-3 g-1 d-flex justify-content-center"  >
-        <% foreach (Dominio.Articulo item in listaLecturaArticulos)
-            {
-        %>
-
-        <div class="col d-flex justify-content-center">
-            <div class="card">
-                <img src="<%=item.ImagenUrl%>" class="card-img-top" alt="Responsive image">
-                <div class="card-body">
-                    <h5 class="card-title" style="color: #E7ECEF"><%=item.Nombre%></h5>
-                    <p class="card-text" style="color: #E7ECEF"><%=item.Descripcion%></p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Precio: $<%=item.Precio%> </small>
-                </div>
-            </div>
-        </div>
-        <%  } %>
+        <div class="row row-cols-1 row-cols-md-3 g-1 d-flex justify-content-center">
+            <asp:Repeater runat="server" ID="repRepetidor">
+                <ItemTemplate>
+                    <div class="col d-flex justify-content-center">
+                        <div class="card">
+                            <img src="<%# Eval("ImagenUrl")%>" class="card-img-top" alt="Responsive image">
+                            <div class="card-body">
+                                <h5 class="card-title" style="color: #E7ECEF"><%#Eval("Nombre")%></h5>
+                                <p class="card-text" style="color: #E7ECEF"><%#Eval("Descripcion")%></p>
+                            </div>
+                            <div class="card-footer">
+                                <asp:Button text="Comprar ahora" CssClass="btn btn-primary" runat="server" ID="btnEjemplo" CommandArgument='<%#Eval("Id")%>' CommandName="ArticuloId" OnClick="btnEjemplo_Click"/>
+                                <br />
+                                <small  style="color:white">Precio: $<%#Eval("Precio")%> </small>
+                            </div>
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
         </div>
     </div>
 </asp:Content>
--- VentanaProductos.aspx.cs
+++ VentanaProductos.aspx.cs
@@ -12,7 +12,9 @@ namespace TPWebForm_equipo_5
     public partial class VentanaProductos : System.Web.UI.Page
     {
         public List<Articulo> listaLecturaArticulos;
+        public Articulo articulosellecionado;
         string busqueda = null;
+        bool cartasfiltradas;
         private void filtrarArticulo(string filtro)
         {
             List<Articulo> listaFiltrada;
@@ -21,10 +23,14 @@ namespace TPWebForm_equipo_5
         }
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-            cargarddl();
-            cargardatos();
-            ordenarcards();
+                cargardatos();
+            if (!IsPostBack)
+            {
+                cargarddl();
+                repRepetidor.DataSource = listaLecturaArticulos;
+                repRepetidor.DataBind();
+            }
+
             busqueda = Request.QueryString["busqueda"];
             if (busqueda != null) filtrarArticulo(busqueda);
         }
@@ -33,10 +39,7 @@ namespace TPWebForm_equipo_5
             LecturaArticulo lecturaArticulo = new LecturaArticulo();
             listaLecturaArticulos = lecturaArticulo.listar();
         }
-        public void ordenarpag(int value)
-        {
-            if (value == 0) { }
-        }
+
 
         public void cargarddl()
         {
@@ -45,18 +48,48 @@ namespace TPWebForm_equipo_5
             DdlOrden.Items.Add("Precio ↓");
         }
 
-        public void ordenarcards()
+        public void modificarlista()
         {
-            List<Articulo> listaFiltrada = new List<Articulo>();
-            if(DdlOrden.SelectedValue == "Precio ↑") {
-               listaFiltrada = listaLecturaArticulos.OrderBy(x => x.Precio).ToList();
-            } //else //if(DdlOrden.SelectedValue == "Precio ↓")
-            listaLecturaArticulos = new List<Articulo>(listaFiltrada);
-            
+
         }
 
-        public void modificarlista() {
+        protected void btnEjemplo_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(((Button)sender).CommandArgument);
             
+            Session.Add("ArticulosEnCarrito", id);
+
+            Response.Redirect("VentanaCarrito.aspx", false);
+
+        }
+
+        protected void DdlOrden_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Articulo> listaFiltrada;
+            cargardatos();
+            listaFiltrada = listaLecturaArticulos;
+
+            if (DdlOrden.SelectedValue == "Precio ↑")
+            {
+                listaFiltrada = listaLecturaArticulos.OrderByDescending(x => x.Precio).ToList();
+                listaLecturaArticulos = listaFiltrada;
+                repRepetidor.DataSource = listaLecturaArticulos;
+                repRepetidor.DataBind();
+            }
+            else if (DdlOrden.SelectedValue == "Precio ↓")
+            {
+                listaFiltrada = listaLecturaArticulos.OrderBy(x => x.Precio).ToList();
+                listaLecturaArticulos = listaFiltrada;
+                repRepetidor.DataSource = listaLecturaArticulos;
+                repRepetidor.DataBind();
+            }
+            else
+            {
+                listaFiltrada = listaLecturaArticulos.OrderBy(x => x.Nombre).ToList();
+                listaLecturaArticulos = listaFiltrada;
+                repRepetidor.DataSource = listaLecturaArticulos;
+                repRepetidor.DataBind();
+            }
         }
     }
 }
