a8eb3c83ec510bdfb72fa9fec41d3a85e45334b3
Santiago Escobar <santiago.escobar97@gmail.com>
2024-05-16 22:24:09 -0300

    Update VentanaDetalle
    Se empezo a agregar las funcionalidades en detalle, por ejemplo la carga de datos mediante el indice que se obtiene de una ventana de productos.

--- VentanaDetalle.aspx.cs
+++ VentanaDetalle.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using Dominio;
+using LecturaDatos;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,9 +12,33 @@ namespace TPWebForm_equipo_5
 {
     public partial class VentanaDetalle : System.Web.UI.Page
     {
+        private List<Articulo> listaLecturaArticulos;
+        private List<Imagen> listaImagenes;
+        int indiceMaximo = 1;
+        int indiceActual = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            LecturaArticulo lecturaArticulo = new LecturaArticulo();
+            listaLecturaArticulos = lecturaArticulo.listar();
+            LecturaImagen lecturaImagen = new LecturaImagen();
+            listaImagenes = lecturaImagen.listar(listaLecturaArticulos[0].Id); //Revisar
+            indiceMaximo = lecturaImagen.maximoImagen(listaLecturaArticulos[0].Id);
+            Articulo seleccionado = new Articulo();
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["id"] != null) //Revisar como esw enviado el id
+                {
+                    int id = Convert.ToInt32(Request.QueryString["id"]);
+                    lecturaImagen.listar(id); //revisar como se envia el id
+                    listaImagenes = lecturaImagen.listar(id);
+                    tbxCantidad.Text = 1.ToString();
+                }
+                lblNombreArticulo.Text = seleccionado.Nombre;
+                lblPrecio.Text = seleccionado.Precio.ToString();
+                lblDescripcion.Text = seleccionado.Descripcion;
+                lblCategoria.Text = seleccionado.Categoria.ToString();
+                lblMarca.Text = seleccionado.Marca.ToString();
+            }
         }
 
         protected void btnComprarAhora_Click(object sender, EventArgs e)
@@ -32,7 +58,7 @@ namespace TPWebForm_equipo_5
 
         protected void btnAgregarCarrito_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void btnEnviarConsulta_Click(object sender, EventArgs e)
